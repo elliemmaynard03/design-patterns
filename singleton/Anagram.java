5e97d4bf8eccff5b2a157d79b3ab62129fc0183a
elliemaynard03 <EMM38@email.sc.edu>
2023-10-01 20:46:45 -0400

    updated final

--- singleton/Anagram.java
+++ singleton/Anagram.java
@@ -1,20 +1,33 @@
 package singleton;
-
+/*
+ * @Author Ellie Maynard
+ * class Anagram calls all of the questions within the anagram files and checks if they are correct
+ */
 import java.util.ArrayList;
+
 
 public class Anagram {
     String question;
     ArrayList<String> answers;
-
+/*
+ * @param String question; takes a question and calls current question
+ * @Param ArrayList<String> answers; takes an answer and calls current answer
+ */
     public Anagram(String question, ArrayList<String> answers) {
         this.question = question;
         this.answers = answers;
     }
 
+    /*
+     * @return question; returns the current question for the user
+     */
     public  String getQuestion() {
         return question;
     }
 
+    /*
+     * @return answers.contains(userAnswer); returns the answer and the answer that the user gives
+     */
     public boolean isCorrect(String userAnswer) {
         return answers.contains(userAnswer);
     }
--- singleton/Game.java
+++ singleton/Game.java
@@ -1,4 +1,8 @@
 package singleton;
+/*
+ * @Author Ellie Maynard
+ * Class Game uses the array lists to allow the user to play the anagram game
+ */
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,7 +15,9 @@ public class Game {
     private static HashMap<Difficulty, ArrayList<Anagram>> anagram = new HashMap<Difficulty, ArrayList<Anagram>>() ;
     private static Anagram currentAnagram;
     private static int score;
-
+/*
+ * uses the file reader and the difficulty enumerations to get a file and add it to the Hash Map 
+ */
     public Game() {
         for (Difficulty difficulty : Difficulty.values()) {
             anagram.put(difficulty, FileReader.getAnagrams(difficulty));
@@ -21,13 +27,21 @@ public class Game {
         rand = new Random();
     }
 
+/*
+ * Get Instance allows a new game to be initialized and played
+ * @return game which returns a new game
+ */
     public static Game getInstance() {
         if (game == null) {
             game = new Game();
         }
         return game;
     }
-
+/*
+ * Allows the user to read a question
+ * Array List levelAnagrams is equal to the anagram array list at the level the user is at
+ * @return currentAnagram.getQuestion which takes the current anagram file's question
+ */
     public String getQuestion() {
         ArrayList<Anagram> levelAnagrams = anagram.get(level);
         if(levelAnagrams == null || levelAnagrams.isEmpty()) {
@@ -37,6 +51,14 @@ public class Game {
         currentAnagram = levelAnagrams.get(index);
        return currentAnagram.getQuestion();
     }
+    /*
+     * Is Correct method says whether or not the word the user puts in a correct anagram
+     * if the answer is correct the score is increased by 1
+     * when the user score is greater than 6 then level is changed to medium
+     * when user score is > than 10 then level is changed to difficult
+     * @param String userAnswer takes the answer the user gives for each anagram question
+     * @return true the method is a boolean expression and starts false but returns true when the method runs
+     */
 
     public boolean isCorrect(String userAnswer) {
         boolean isAnswerCorrect = false;
@@ -61,6 +83,9 @@ public class Game {
         return true;
     }
 
+    /*
+     * @return this.getScore() which gets the user score 
+     */
     public int getScore() {
         return this.getScore();
     }
